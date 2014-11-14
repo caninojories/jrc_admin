b0425ab50c732cc8ff222f2f275d5b5cfa8f51b2
canino_jories <canino_jories@hotmail.com>
2014-11-14 18:09:17 +0800

    add questions

--- front-end/resources/js/paragala/questionsForClient.js
+++ front-end/resources/js/paragala/questionsForClient.js
@@ -14,6 +14,7 @@
 
       vm.questionBuilder  = [];
       vm.questionList     = null;
+      vm.counterSelected  = true;
 
       vm.selectedQuestion     = selectedQuestion;
       vm.studentLogout        = studentLogout;
@@ -32,6 +33,14 @@
               vm.previousUrl = response.previousUrl;
               vm.nextUrl = response.nextUrl;
               vm.sliceObject = response.questions;
+              console.log( vm.sliceObject[0].items.length )
+              for( var i = 0; i < vm.sliceObject[0].items.length; i++ ) {
+                console.log( vm.sliceObject[0].items[i] )
+                // for( var j = 0; j < vm.sliceObject[0].items.length; j++) {
+                //   console.log( 'jories' )
+                // }
+              }
+
             })
         }
 
@@ -77,23 +86,15 @@
 
           for( var i = 0; i < $rootScope.list.length; i++) {
             if($rootScope.list[i].title == categoryTitle ) {
-              console.log( $rootScope.list[i].title )
               for( var j = 0; j <$rootScope.list[i].items.length; j++ ) {
                 if($rootScope.list[i].items[j].title == subItemTitle ) {
-                  console.log( $rootScope.list[i].items[j].title )
                   for( var z = 0; z < $rootScope.list[i].items[j].items.length; z++ ) {
-                      console.log( $rootScope.list[i].items[j].items[0] )
-                      console.log( i )
-                      console.log( j )
-                      console.log( subItemTitle )
-                      vm.questionBuilder[i].items[j].items[0].title = SecondSubItemTitle
-                      vm.questionBuilder[i].items[j].items[0].selected = true;
-                      console.log( vm.questionBuilder )
-                      $q.all( selectedQuestionData() )
-                        .then(function( response ) {
-                          console.log( response )
-                        })
-
+                    vm.questionBuilder[i].items[j].items[0].title = SecondSubItemTitle
+                    vm.questionBuilder[i].items[j].items[0].selected = true;
+                    $q.all( selectedQuestionData() )
+                      .then(function( response ) {
+                        console.log( response )
+                      })
                   }
                 }
               }
@@ -104,7 +105,6 @@
         function selectedQuestionData() {
           return paragalaDataservice.selectedQuestion( 'questionsListClient', {questions: angular.toJson(vm.questionBuilder)} )
             .then(function( response ) {
-              console.log( response )
               return response;
             })
         }
@@ -125,14 +125,18 @@
             })
         }
 
-        function dataQuestionSelected( categoryTitle, subItemTitle, SecondSubItemTitle ) {
+        function dataQuestionSelected( categoryTitle, subItemTitle, secondSubItemTitle ) {
           if( $rootScope.studentQuestionList == undefined ) return 0;
           for( var i = 0; i < $rootScope.studentQuestionList.length; i++) {
             if($rootScope.studentQuestionList[i].title == categoryTitle ) {
               for( var j = 0; j < $rootScope.studentQuestionList[i].items.length; j++ ) {
                 if($rootScope.studentQuestionList[i].items[j].title == subItemTitle ) {
                   for( var z = 0; z < $rootScope.studentQuestionList[i].items[j].items.length; z++ ) {
-                    if($rootScope.studentQuestionList[i].items[j].items[z].title == SecondSubItemTitle ) {
+                    // if(  $rootScope.studentQuestionList[i].items[0].items[z].selected == true
+                    //   &&  $rootScope.studentQuestionList[i].items[1].items[z].selected == true) {
+                    //     vm.counterSelected = false;
+                    //   }
+                    if($rootScope.studentQuestionList[i].items[j].items[z].title == secondSubItemTitle ) {
                       return true;
                     }
                   }
